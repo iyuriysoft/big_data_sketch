4921a23196beb5750f9aabe33cc6a04e0a11f28a
iyuriysoft <42405281+iyuriysoft@users.noreply.github.com>
2018-09-25 19:09:20 +0300

    Add files via upload

--- HiveSQL/select_63_task.sql
+++ HiveSQL/select_63_task.sql
@@ -1,10 +1,3 @@
--- using UDF (change according your jar-name)
-delete jar /home/cloudera/my/b-0.0.1.jar;
-add jar /home/cloudera/my/b-0.0.1.jar;
-create temporary function getStartIP as 'a.udf.GetStartIP';
-create temporary function getEndIP as 'a.udf.GetEndIP';
-create temporary function getIP as 'a.udf.GetIP';
-
 -- create view Product with ip as long
 create view if not exists vproduct as 
 select tb.ip, getIP(tb.ip) as iip, tb.p as sump
